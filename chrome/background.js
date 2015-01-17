993ec0deb1c382f91b8b35cebf49adcea9b205df
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-01-17 16:44:58 -0500

    toggle and get page contents

--- chrome/background.js
+++ chrome/background.js
@@ -2,19 +2,28 @@ var isOn = [];
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     if (isOn[tab.id]) {
-        chrome.browserAction.setIcon({tabId: tab.id, path: "action@2x.png"});
+        chrome.browserAction.setIcon({tabId: tab.id, path: {"19": "off.png", "38": "off@2x.png"} });
         isOn[tab.id] = false;
+        //refreshes the page
+        chrome.tabs.getSelected(null, function(tab) {
+            var code = 'window.location.reload();';
+            chrome.tabs.executeScript(tab.id, {code: code});
+        });
     } else {
-        chrome.browserAction.setIcon({tabId: tab.id, path: { "19": "Logo.png", "38": "Logo.png" }});
+        chrome.browserAction.setIcon({tabId: tab.id, path: {"19": "on.png", "38": "on@2x.png"} });
         isOn[tab.id] = true;
+        //get page contents
+        getPageContents(function(res) {
+            console.log(res.data);
+        })
     }
 });
 
 
-function getPageContents() {
+function getPageContents(cb) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-            console.log();
+        chrome.tabs.sendMessage(tabs[0].id, {data: "getPageContents"}, function(response) {
+            cb(response);
         });
     });
 }
--- chrome/script.js
+++ chrome/script.js
@@ -91,10 +91,7 @@
 }());
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-        "from a content script:" + sender.tab.url :
-        "from the extension");
-    if (request.greeting == "hello") {
-        sendResponse({farewell: "goodbye"});
+    if (request.data == "getPageContents") {
+        sendResponse({"data": "bullshit"});
     }
 });
