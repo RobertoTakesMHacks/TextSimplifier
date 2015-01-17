78a93ad6314ca84f6c69db2634e574e6a9e8944c
jkbenj <julietkbenjamin@gmail.com>
2015-01-17 15:15:51 -0500

    Creates the tree

---
+++ server/grammar.py
@@ -0,0 +1,33 @@
+import nltk
+
+#paragraph = "The unicorn is a legendary animal that has been described since antiquity as a beast with a large, pointed, spiraling horn projecting from its forehead. The unicorn was depicted in ancient seals of the Indus Valley Civilization and was mentioned by the ancient Greeks in accounts of natural history by various writers, including Ctesias, Strabo, Pliny the Younger, and Aelian."
+#paragraph = "If it can't match the rest of the pattern, \{(12 backtracks,)] each time discarding one of the matches until it can either match the entire pattern or be certain that it cannot get a match."
+#paragraph = "You can iterate pretty much anything in python using the for loop construct, for example, open('file.txt') returns a file object (and opens the file), iterating over it iterates over lines in that file \t for line in open(filename): # do something with line If that seems like magic, well it kinda is, but the idea behind it is really simple."
+
+# GRAMMAR
+# NP: noun phrase - contains a determinant or adjectives with nouns OR proper noun(s)
+# NPX: list OR noun phrase involving participals used as adjectives
+# VP: verb phrase - contains a determinant or adverbs with verbs
+# VPX: deals with verb phrases such as "be certain" or "be sure"
+# VPX1: deals with statements like "either x or y" where x or y are accidentally marked as nouns
+# VPX1: {<VP>?(<NP>|<NPX>)?(<NP>|<NPX>)*<CC>+}
+# see http://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html and http://www.nltk.org/book/ch07.html for more information
+grammar = r"""
+    NP: {(<DT>|<PDT>)?(<CD>)?(<JJ>|<JJR>|<JJS>)*(<NN>|<NNS>|<NNP>|<NNPS>|<CD>|(<ORGANIZATION>|<PERSON>|<LOCATION>|<DATE>|<TIME>|<MONEY>|<PERCENT>|<FACILITY>|<GPE>))+<,>*}
+        {(<DT>|<PDT>)+(<JJ>|<JJR>|<JJS>)+<,>*}
+        {<PRP>}
+    VP: {(<DT>|<PDT>)?(<RB>|<RBR>|<RBS>)*(<MD><RB>)?(<VB>|<VBD>|<VBG>|<VBN>|<VBP>|<VBZ>)+<,>*}
+
+    NPX: {<NP>*<,>+<VP>+(<,>|<NP>)*}
+    VPX: {<VP>+(<JJ>|<JJR>|<JJS>)+}
+    """
+
+def parse(text):
+    tokens = nltk.word_tokenize(text)
+    tagged = nltk.pos_tag(tokens)
+    entities = nltk.ne_chunk(tagged)
+    resultTree = nltk.RegexpParser(grammar).parse(entities)
+
+    return resultTree
+
+parse(paragraph).draw()
