6cfcb200d19a4bfd9b42e853a100e0e9abd9d876
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-01-17 16:33:00 -0500

    initial toggleable functionality

--- chrome/background.js
+++ chrome/background.js
@@ -1,11 +1,20 @@
-var isOn = false;
+var isOn = [];
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    inOn = !isOn;
-
-    if (isOn) {
-
+    if (isOn[tab.id]) {
+        chrome.browserAction.setIcon({tabId: tab.id, path: "action@2x.png"});
+        isOn[tab.id] = false;
     } else {
-        
+        chrome.browserAction.setIcon({tabId: tab.id, path: { "19": "Logo.png", "38": "Logo.png" }});
+        isOn[tab.id] = true;
     }
 });
+
+
+function getPageContents() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+            console.log();
+        });
+    });
+}
--- chrome/script.js
+++ chrome/script.js
@@ -89,3 +89,12 @@
 
     window.addEventListener('load', startWalking);
 }());
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+        "from a content script:" + sender.tab.url :
+        "from the extension");
+    if (request.greeting == "hello") {
+        sendResponse({farewell: "goodbye"});
+    }
+});
