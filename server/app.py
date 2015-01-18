c2a9acbf24463bf79f574e5875362b009bfd663c
Maximillian von Briesen <mobyvb@gmail.com>
2015-01-17 19:38:12 -0500

    get raw text from html

--- server/app.py
+++ server/app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request, url_for
 from replacecommonphrases import replace_common_phrases
+from urllib import urlopen
 app = Flask(__name__)
+import nltk
+from bs4 import BeautifulSoup
 
 paragraph = "The unicorn is a legendary animal that has been described since antiquity as a beast with a large, pointed, spiraling horn projecting from its forehead. The unicorn was depicted in ancient seals of the Indus Valley Civilization and was mentioned by the ancient Greeks in accounts of natural history by various writers, including Ctesias, Strabo, Pliny the Younger, and Aelian."
 
 @app.route('/', methods=['POST'])
 def handle_data():
-    text = request.data
-    print text
-    new_text = replace_common_phrases(text)
+    html = request.data
+    soup = BeautifulSoup(html)
+    paragraphs = soup.find_all('p')
+    new_text = ''
+    for paragraph in paragraphs:
+        new_text += replace_common_phrases(paragraph.getText()) + '<br><br>'
     print new_text
     return new_text
 
